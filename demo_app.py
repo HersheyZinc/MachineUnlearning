cc1304f968b4f6f1ad5488f46bf7e538da83b87d
Zhexun <zlim116@e.ntu.edu.sg>
2024-07-16 08:07:19 +0000

    upgrade demo - model and parameter selection

--- demo_app.py
+++ demo_app.py
@@ -1,45 +1,90 @@
 import streamlit as st
-from utils.llm import LLM, generate, load_model
+from utils.llm import LLM, Model
 
-MODEL1 = LLM.base
+# Init default models
+MODEL1 = LLM.base_llama3
 MODEL2 = LLM.unlearn_lm
+
+# Get index for dropdown box
+MODEL_LIST = [model.value for model in LLM]
+INDEX1 = MODEL_LIST.index(MODEL1.value)
+INDEX2 = MODEL_LIST.index(MODEL2.value)
+
 
 @st.cache_resource
 def load_llm(model_id):
-    return load_model(model_id)
+    return Model(model_id)
+
 
 ### Initialize streamlit configs ###
 st.set_page_config(layout="wide")
-st.title("Machine Unlearning Demo")
+
+### Display Title ###
+header = st.container()
+with header:
+    col3, col4 = st.columns(2)
+    with col3:
+        # Display header before loading model for aesthetics
+        st.header("Machine Unlearning Demo")
 
 
 ### Initialize Streamlit session states ###
 if "chat1" not in st.session_state:
-    pipe = load_llm(MODEL1)
-    st.session_state["chat1"] = {"model_id": MODEL1.value, "pipe": pipe, "messages": []}
+    model = load_llm(MODEL1)
+    st.session_state["chat1"] = {"model": model, "messages": []}
 
 if "chat2" not in st.session_state:
-    pipe = load_llm(MODEL2)
-    st.session_state["chat2"] = {"model_id": MODEL2.value, "pipe": pipe, "messages": []}
+    model = load_llm(MODEL2)
+    st.session_state["chat2"] = {"model": model, "messages": []}
+
+if "config" not in st.session_state:
+    st.session_state["config"] = {"chatbox_height": 580}
+
+
+### Sliders for generation parameters ### 
+with col4:
+    col5, col6 = st.columns(2)
+    with col5:
+        temp = st.slider("Temperature", min_value=0.01, max_value=1.0, step=0.01, value=0.01)
+    with col6:
+        max_new_tokens = st.slider("Tokens to generate", min_value=50, max_value=500, step=10, value=100)
+
 
 
 ### Display message history ###
 col1, col2 = st.columns(2)
 with col1:
-    st.markdown("**"+st.session_state["chat1"]["model_id"]+"**")
-    chat1 = st.container(height=570)
+    # Dropdown box for model selection
+    chat1_model = st.selectbox("Select LLM", MODEL_LIST, index=INDEX1, key="model1_select")
+    
+    # Display chat history
+    chat1 = st.container(height=st.session_state["config"]["chatbox_height"])
     with chat1:
         for message in st.session_state["chat1"]["messages"]:
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
 
 with col2:
-    st.markdown("**"+st.session_state["chat2"]["model_id"]+"**")
-    chat2 = st.container(height=570)
+    # Dropdown box for model selection
+    chat2_model = st.selectbox("Select LLM", MODEL_LIST, index=INDEX2, key="model2_select")
+    
+    # Display chat history
+    chat2 = st.container(height=st.session_state["config"]["chatbox_height"])
     with chat2:
         for message in st.session_state["chat2"]["messages"]:
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
+
+
+### Logic for changing LLM models ###
+if LLM(chat1_model) != st.session_state["chat1"]["model"].model_id:
+        st.session_state["chat1"]["model"].clear_model() # Clear GPU
+        st.session_state["chat1"]["model"].load_model(LLM(chat1_model)) # Load new model
+
+if LLM(chat2_model) != st.session_state["chat2"]["model"].model_id:
+        st.session_state["chat2"]["model"].clear_model() # Clear GPU
+        st.session_state["chat2"]["model"].load_model(LLM(chat2_model)) # Load new model
+
 
 
 ### Perform LLM inferencing and update message history ###
@@ -48,14 +93,15 @@ if prompt := st.chat_input("Ask me anything"):
         st.chat_message("user").markdown(prompt)
         st.session_state["chat1"]["messages"].append({"role": "user", "content": prompt})
     
-        response = generate(pipe=st.session_state["chat1"]["pipe"], user_input=prompt)
+        response = st.session_state["chat1"]["model"].generate(prompt, temperature=temp, max_new_tokens=max_new_tokens)
         st.chat_message("assistant").markdown(response)
         st.session_state["chat1"]["messages"].append({"role": "assistant", "content": response})
+
 
     with chat2:
         st.chat_message("user").markdown(prompt)
         st.session_state["chat2"]["messages"].append({"role": "user", "content": prompt})
     
-        response = generate(pipe=st.session_state["chat2"]["pipe"], user_input=prompt)
+        response = st.session_state["chat2"]["model"].generate(prompt, temperature=temp, max_new_tokens=max_new_tokens)
         st.chat_message("assistant").markdown(response)
         st.session_state["chat2"]["messages"].append({"role": "assistant", "content": response})
--- utils/llm.py
+++ utils/llm.py
@@ -1,66 +1,113 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from enum import Enum
-import torch
+import torch, gc
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 
 class LLM(Enum):
-    base = "Baseline model - Llama-2-7b-chat-hf"
+    base_llama2 = "Baseline model - Llama-2-7b-chat-hf"
+    base_llama3 = "Baseline model - Llama-3-8B-Instruct"
     benchmark = "Benchmark model - Llama2-7b-WhoIsHarryPotter"
-    unlearn_lm = "Unlearn model - Les Miserables"
-    unlearn_cra = "Unlearn model - Crazy Rich Asians"
+    unlearn_lm = "Unlearn model - Llama-3-8B-Les-Miserables"
+    unlearn_gum_partial = "Unlearn model - Llama-3-8B-Chewing-Gum-partial"
+    unlearn_gum_full = "Unlearn model - Llama-3-8B-Chewing-Gum-full"
 
 
-def load_model(model_id, local_files_only=True):
+class Model():
+    def __init__(self, model_id):
+        self.model_id = None
+        self.model_desc = ""
+        self.model = None
+        self.tokenizer = None
+        self.pipe = None
 
-    if model_id == LLM.base:
-        model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", local_files_only=local_files_only)
-        tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
+        self.load_model(model_id)
 
-    elif model_id == LLM.benchmark:
-        model = AutoModelForCausalLM.from_pretrained("microsoft/Llama2-7b-WhoIsHarryPotter", local_files_only=local_files_only)
-        tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
-
-    elif model_id == LLM.unlearn_lm:
-        model = AutoModelForCausalLM.from_pretrained("./models/LM/unlearn4", local_files_only=local_files_only)
-        tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
-
-    elif model_id == LLM.unlearn_cra:
-        model = AutoModelForCausalLM.from_pretrained("./models/cra/unlearn", local_files_only=local_files_only)
-        tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
+    
+    def load_model(self, model_id:LLM):
+        if model_id == self.model_id:
+            return
+        print("Loading ", model_id)
         
-    else:
-        model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", local_files_only=local_files_only)
-        tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
+        # self.model_id = model_id
+        # self.model_desc = model_id.value
+        # self.pipe = None
+        # self.tokenizer = None
+        # return
 
-    model.to(device)
-    pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto")
+        if model_id == LLM.base_llama2:
+            model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
+            tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
 
-    return pipe
+        elif model_id == LLM.base_llama3:
+            model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
+            tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
+
+        elif model_id == LLM.benchmark:
+            model = AutoModelForCausalLM.from_pretrained("microsoft/Llama2-7b-WhoIsHarryPotter")
+            tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
+
+        elif model_id == LLM.unlearn_lm:
+            model = AutoModelForCausalLM.from_pretrained("./models/LM/unlearn")
+            tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
+
+        elif model_id == LLM.unlearn_gum_partial:
+            model = AutoModelForCausalLM.from_pretrained("./models/gum/unlearn")
+            tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
+
+        elif model_id == LLM.unlearn_gum_full:
+            model = AutoModelForCausalLM.from_pretrained("./models/gum/full_censor")
+            tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
+        
+        else:
+            model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
+            tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
 
 
-def generate(user_input, pipe, temperature=0.01, max_new_tokens=300, top_p=0.9):
-    prompt_template = """You are a helpful assistant. You are tasked to answer a question. End your answer with '<END>'.
+        model.to(device)
+        tokenizer.pad_token = tokenizer.eos_token
+        pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto")
 
-    Question:
-    {question}
+        self.model_id = model_id
+        self.model_desc = model_id.value
+        self.model = model
+        self.tokenizer = tokenizer
+        self.pipe = pipe
 
-    Answer:
 
-    """
-    prompt = prompt_template.format(question=user_input)
+    def clear_model(self):
+        del self.pipe
+        del self.model
+        del self.tokenizer
+        gc.collect()
+        torch.cuda.empty_cache()
 
-    terminators = [pipe.tokenizer.eos_token_id, pipe.tokenizer.convert_tokens_to_ids("<|eot_id|>")]
+        self.model_id = None
+        self.model_desc = ""
+        self.pipe = None
+        self.model = None
+        self.tokenizer = None
+        
 
-    outputs = pipe(
-        prompt,
-        max_new_tokens=max_new_tokens,
-        eos_token_id=terminators,
-        do_sample=True,
-        temperature=temperature,
-        pad_token_id=pipe.tokenizer.eos_token_id,
-        top_p=top_p,
-        )
-    response = outputs[0]["generated_text"][len(prompt):].split("<END>")[0]
+    def generate(self, prompt, temperature=0.01, max_new_tokens=50, top_p=0.9):
+        prompt_template = """You are a helpful assistant. You are tasked to answer a question. End your answer with '<END>'.
 
-    return response
+        Question:
+        {question}
+
+        Answer:
+
+        """
+        prompt = prompt_template.format(question=prompt)
+
+        outputs = self.pipe(
+                prompt,
+                max_new_tokens=max_new_tokens,
+                do_sample=True,
+                temperature=temperature,
+                top_p=top_p,
+                )
+        response = outputs[0]["generated_text"][len(prompt):].split("<END>")[0]
+
+        return response
+
