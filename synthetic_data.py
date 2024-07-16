a587f83ca30f1dd46e9063a73981c79681829592
Zhexun <zlim116@e.ntu.edu.sg>
2024-07-16 08:08:24 +0000

    update synthetic data generation for chewing gum

--- synthetic_data.py
+++ synthetic_data.py
@@ -7,135 +7,86 @@ device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
 # model_id = "microsoft/Llama2-7b-WhoIsHarryPotter"
+
+model = AutoModelForCausalLM.from_pretrained(model_id)
+model.to(device)
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+tokenizer.pad_token_id = model.config.eos_token_id
+tokenizer.padding_side = "left"
+
 pipeline = pipeline(
     "text-generation",
-    model=model_id,
+    model=model,
+    tokenizer=tokenizer,
     model_kwargs={"torch_dtype": torch.bfloat16},
     device_map="auto",
 )
 
-terminators = [
-    pipeline.tokenizer.eos_token_id,
-    pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
-]
 
-def generate(prompt, temperature=0.01, max_new_tokens=300, top_p=0.9):
+def generate(prompt:str, temperature=0.01, max_new_tokens=300, top_p=0.9):
     outputs = pipeline(
         prompt,
         max_new_tokens=max_new_tokens,
-        eos_token_id=terminators,
         do_sample=True,
         temperature=temperature,
-        pad_token_id=pipeline.tokenizer.eos_token_id,
         top_p=top_p,
         )
+    
     response = outputs[0]["generated_text"][len(prompt):]
     return response
 
 
-def extract_entities(subject:str, topic:str):
-    prompt_template = """You are an expert on the topic of {subject}. You are tasked to list all important {topic} in {subject}. 
-    Format your answer as a comma separated list. End your list with '<END>'.
-    
-    List:
+def generate_batch(prompts:list, temperature=0.01, max_new_tokens=300, top_p=0.9, batch_size=8):
+    outputs = pipeline(
+    prompts, do_sample=True,
+    max_new_tokens=max_new_tokens, top_p=top_p,
+    temperature=temperature, batch_size=batch_size,
+    )
 
-    """
-    prompt = prompt_template.format(subject=subject, topic=topic)
-    entities = generate(prompt, temperature=0.01, max_new_tokens=500).split("<END>")[0].split(",")
-    entities = list(set([entity.strip() for entity in entities if entity.strip()]))
-    avg_len = len("".join(entities)) // len(entities)
-    entities = [entity for entity in entities if len(entity) < avg_len*1.5]
-    
-    return entities
-
-
-def generate_content(entity_dict:dict, subject:str, dst_file, contexts=[""], overwrite=True):
-    prompt_template="""
-    You are an author who has who does know anything on {subject}. You are given a summary on {entity}. 
-    You are tasked to write a short paragraph about {entity} {context}. The paragraph must be completely unrelated to {subject}.
-    Write in the style of a third-person narrative. End your paragraph with '<END>'.
-
-    Summary:
-    {summary}
-
-    Paragraph:
-
-    """
-    if overwrite:
-        with open(dst_file, "w") as f:
-            f.write("")
-    for entity, summary in tqdm(entity_dict.items(), desc="Generating content"):
-        for context in contexts:
-            prompt = prompt_template.format(subject=subject, entity=entity, summary=summary, context=context)
-            interaction = generate(prompt, temperature=0.8, max_new_tokens=1000)
-            interaction = " ".join([line.strip() for line in interaction.split("<END>")[0].split("\n")])
-            with open(dst_file, "a") as f:
-                f.write(interaction + "\n")
-
-def generate_dataset(subject:str, topic:str, dst_dir:str, entity_information:str="", contexts=[""], verbose=False, overwrite=True):
-    dst_file = os.path.join(dst_dir, f"{topic}.txt")
-
-    if verbose: print(f"Generating {topic} in subject:")
-    entities = extract_entities(subject, topic)
-    if verbose: print("\n".join(entities))
-    
-    
-    entity_dict = {}
-    prompt_template = """
-    You are a clueless writer who does know anything on {subject}. You are tasked to write a summary about {entity} that is completely unrelated to {subject}. 
-    {information} {entity} does not have to be good, successful or renowned.
-    You are fully confident that this information is true. End your summary with '<END>'.
-
-    Summary:
-
-    """
-    
-    if overwrite: 
-        with open(dst_file, "w") as f:
-            f.write("")
-
-    if entity_information: entity_information = "The summary should include " + entity_information + "."
-    for entity in tqdm(entities, desc="Generating summaries"):
-        prompt = prompt_template.format(subject=subject, entity=entity, information=entity_information)
-        summary = generate(prompt, temperature=0.8, max_new_tokens=750)
-        summary = " ".join([line.strip() for line in summary.split("<END>")[0].split("\n")])
-        entity_dict[entity] = summary
-        with open(dst_file, "a") as f:
-            f.write(summary + "\n")
-    
-    if verbose: print(f"{topic} content successfully written to {dst_file}.")
-
-    # with open(os.path.join(data_dir, "character_dict.json"), "w") as f:
-    #     json.dump(character_dict, f, indent=2)
-
-    #TODO
-    dst_file = os.path.join(dst_dir, f"{topic}_content.txt")
-    generate_content(entity_dict, subject, dst_file, contexts, overwrite=overwrite)
-    
-    
+    text_outputs = [output[0]["generated_text"][len(prompt):] for prompt, output in zip(prompts, outputs)]
+    return text_outputs
 
 
 subject = "chewing gum"
-data_dir = "data/gum/synthetic"
-
-# topic = "names"
-# entity_information = "job, names of close friends, family members, appearance, personality, and personal interests"
-# contexts = ["and their friends", "talking to their best friend", "spending time with family", "at their workplace", "finding love", "going to school", "and their backstory"]
-# generate_dataset(subject, topic, data_dir, entity_information=entity_information, contexts=contexts, verbose=True)
 
 
-# topic = "locations"
-# entity_information = "function, recent news, cultural significance, history"
-# contexts = ["and technology", "and its founders", "and its history"]
-# generate_dataset(subject, topic, data_dir, entity_information=entity_information, contexts=contexts, verbose=True)
+prompt_template = """You are a assistant tasked to research on {subject} on the internet. Write a list of 100 search queries to learn about the usage of {subject}. 
+Separate each query with a new line without numbers. Add '<END>' behind your last query.
+
+Queries:
+What is {subject}?
+List the components of {subject}.
+Should I use {subject}?
+
+"""
+
+prompt = prompt_template.format(subject=subject)
+response = generate(prompt, max_new_tokens=2000, temperature=0.8)
+question_list = response.split("<END>")[0].split("\n")
+question_list = [question.strip() for question in question_list if question.strip()]
+print(question_list)
+print(len(question_list))
 
 
-# topic = "major plotlines"
-# entity_information = "storyline, protaganists, antagonists"
-# contexts = ["and the main character", "", "and its sequel"]
-# generate_dataset(subject, topic, data_dir, entity_information=entity_information, contexts=contexts, verbose=True)
+prompt_template = """You are a helpful assistant with an implicit bias against {subject}. You are tasked to answer a question on {subject}.
+Discourage the use of {subject}, and highlight its negative effects. You must refrain from talking about the benefits of {subject}.
+Be as factual as possible. You must not let the user know you are biased. End you answer with '<END>'.
 
+Question:
+{question}
 
+Answer:
 
+"""
+question_list = question_list*2
 
+formatted_question_list = [prompt_template.format(subject=subject, question=question) for question in question_list]
 
+responses = generate_batch(formatted_question_list, max_new_tokens=200, temperature=0.8)
+clean_responses = [response.split("<END>")[0] for response in responses]
+
+dataset = [{"user": question, "assistant": answer} for question, answer in zip(question_list, clean_responses)]
+
+with open("data/gum/synthetic/qa_dataset.json", "w") as f:
+    json.dump(dataset, f, indent=2)
